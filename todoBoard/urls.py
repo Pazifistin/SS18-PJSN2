6510795d8f838dd5356f720a3edbb90c9152dffd
8dzed2 <38781742+8dzed2@users.noreply.github.com>
2018-05-10 20:23:43 +0200

    added functionality to todoBoard tried to add functionality to create and edit todo sites (still not finished)

--- todoBoard/urls.py
+++ todoBoard/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'todoBoard'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
-    re_path(r'^(?P<pk>[0-9]+)/$', views.edit, name='edit'),
+    path('new/new/', views.newsubmit, name='newSubmit'),
+    re_path(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
+    re_path(r'^edit/(?P<pk>[0-9]+)/delete/$', views.delete, name='delete'),
 ]
--- todoBoard/views.py
+++ todoBoard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Todo
 
@@ -9,13 +9,23 @@ def index(request):
     return render(request, 'todoBoard/index.html', {'all_todos': all_todos})
 
 
+# returns the createTodo.html
 def new(request):
     return render(request, 'todoBoard/createTodo.html')
 
 
+# gets called when the submit button is hit on the createTodo.html
+def newsubmit(request):
+    description = request.POST['description']
+    return redirect('')
+
+
 def edit(request, pk):
-    try:
-        todo = Todo.objects.get(id=pk)
-    except Todo.DoesNotExist:
-        raise Http404("Todo does not exist")
+    todo = get_object_or_404(Todo, id=pk)
     return render(request, 'todoBoard/editTodo.html', {'todo': todo})
+
+
+def delete(request, pk):
+    todo = get_object_or_404(Todo, id=pk)
+    Todo.objects.filter(id=pk).delete()
+    return redirect('')
